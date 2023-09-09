7ab793ced1efd926226b9f90ab30d51617acf64c
Vicente-G <vicentegonsal2000@gmail.com>
2023-09-09 17:17:20 -0300

    fix: solved problem with routes sending bad json responses

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -5,11 +5,11 @@ from .process import response as process_response
 
 def router(app: Flask) -> Flask:
     @app.route("/status", methods=["GET"])
-    def status() -> Response:
-        return Response(None, status=200)
+    def status() -> tuple[Response, int]:
+        return Response(None), 200
 
     @app.route("/process", methods=["POST"])
-    def process() -> Response:
+    def process() -> tuple[Response, int]:
         return process_response(request)
 
     return app
